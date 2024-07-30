74b75fe5a16bdd926b5a842072d3afd825359a87
Florent GUITTRE <florent.guittre@epitech.eu>
2024-07-30 17:41:21 +0200

    feat: add hatching and gradient in circle

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ async function init() {
 function createPipeline(device, format) {
     const vertexShaderCode = `
         @vertex
-        fn main(@builtin(vertex_index) vertexIndex : u32) -> @builtin(position) vec4<f32> {
+        fn main(@builtin(vertex_index) index : u32) -> @builtin(position) vec4<f32> {
             var pos = array<vec2<f32>, 6>(
                 vec2<f32>(-1.0, -1.0),
                 vec2<f32>(1.0, -1.0),
@@ -30,43 +30,53 @@ function createPipeline(device, format) {
                 vec2<f32>(1.0, -1.0),
                 vec2<f32>(1.0, 1.0)
             );
-            return vec4<f32>(pos[vertexIndex], 0.0, 1.0);
+            return vec4<f32>(pos[index], 0.0, 1.0);
         }`;
 
     const fragmentShaderCode = `
         @fragment
         fn main(@builtin(position) FragCoord : vec4<f32>) -> @location(0) vec4<f32> {
-            let brightPurple = vec3<f32>(0.5255, 0.4275, 1.1);
-            let darkPurple = vec3<f32>(0.0, 0.0, 0.5176); 
-            let t = FragCoord.y / 480.0;
-            let gradient = mix(brightPurple, darkPurple, t);
-            
+            let height = 480.0;
+            let topBackgroundColor = vec3<f32>(0.5255, 0.4275, 1.0);
+            let bottomBackgroundColor = vec3<f32>(0.0, 0.0, 0.5176);
+            let topCircleColor = vec3<f32>(1.0, 0.8118, 0.3255);
+            let bottomCircleColor = vec3<f32>(1.0, 0.0, 0.9882);
             let circleCenter = vec2<f32>(0.5, 0.5);
-            let circleRadius = 0.2;
-            let fragCoordNormalized = FragCoord.xy / vec2<f32>(480.0, 480.0);
-            let dist = length(fragCoordNormalized - circleCenter);
-            
-            let circleColor = vec3<f32>(1.0, 0.8118, 0.3255);
+            let circleRadius = 0.375;
             var color = vec4<f32>(0.0, 0.0, 0.0, 1.0);
             
-            if (dist < circleRadius) {
-                color = vec4<f32>(circleColor, 1.0);
+            let backgroundT = FragCoord.y / height;
+            let gradientBackground = mix(topBackgroundColor, bottomBackgroundColor, backgroundT);
+
+            let normalizedCoordinates = FragCoord.xy / vec2<f32>(height, height);
+            let distance = length(normalizedCoordinates - circleCenter);
+
+            if (distance < circleRadius) {
+                let dynamicYield = min(50.0, FragCoord.y / 20.0);
+                let circleT = FragCoord.y / height / 1.2;
+                let gradientCircle = mix(topCircleColor, bottomCircleColor, circleT);
+                if (FragCoord.y > (height / 2.5)) {
+                    if (FragCoord.y % 42.0 > dynamicYield) {
+                        color = vec4<f32>(gradientCircle, 1.0);
+                    } else {
+                        color = vec4<f32>(gradientBackground, 1.0);
+                    }
+                } else {
+                    color = vec4<f32>(gradientCircle, 1.0);
+                }
             } else {
-                color = vec4<f32>(gradient, 1.0);
+                color = vec4<f32>(gradientBackground, 1.0);
             }
             return color;
         }`;
 
-    const vertexModule = device.createShaderModule({ code: vertexShaderCode });
-    const fragmentModule = device.createShaderModule({ code: fragmentShaderCode });
-
     return device.createRenderPipeline({
         vertex: {
-            module: vertexModule,
+            module: device.createShaderModule({ code: vertexShaderCode }),
             entryPoint: 'main',
         },
         fragment: {
-            module: fragmentModule,
+            module: device.createShaderModule({ code: fragmentShaderCode }),
             entryPoint: 'main',
             targets: [{ format: format }],
         },
@@ -91,13 +101,11 @@ async function render(device, context, pipeline) {
     pass.setPipeline(pipeline);
     pass.draw(6, 1, 0, 0);
     pass.end();
-
     device.queue.submit([encoder.finish()]);
 }
 
 async function main() {
     const { device, context, canvasFormat } = await init();
-
     const pipeline = createPipeline(device, canvasFormat);
 
     function frame() {
