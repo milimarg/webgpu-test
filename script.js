39a60420ce90c26259753d0f155a3ac26dcc2a29
Florent GUITTRE <florent.guittre@epitech.eu>
2024-08-11 00:59:48 +0200

    fix: use SDF boolean operators instead of ifs

--- script.js
+++ script.js
@@ -34,38 +34,57 @@ function createPipeline(device, format) {
         }`;
 
     const fragmentShaderCode = `
+        fn computeSDFSphere(p : vec3<f32>, o : vec3<f32>, s : f32) -> f32 {
+            return length(p - o) - s;
+        }
+        
+        fn computeSDFRectangle(p : vec3<f32>, o : vec3<f32>, b : vec3<f32>) -> f32 {
+            let q : vec3<f32> = abs(p - o) - b;
+            return length(max(q, vec3<f32>(0.0, 0.0, 0.0))) + min(max(q.x, max(q.y, q.z)), 0.0);
+        }
+        
+        fn computeBackgroundColor(y : f32, height : f32, topColor : vec3<f32>, bottomColor : vec3<f32>) -> vec4<f32> {
+            let backgroundT = y / height;
+            let gradientBackground = mix(topColor, bottomColor, backgroundT);
+            return vec4<f32>(gradientBackground, 1.0);
+        }
+
         @fragment
         fn main(@builtin(position) FragCoord : vec4<f32>) -> @location(0) vec4<f32> {
-            let height = 480.0;
-            let topBackgroundColor = vec3<f32>(0.5255, 0.4275, 1.0);
-            let bottomBackgroundColor = vec3<f32>(0.0, 0.0, 0.5176);
-            let topCircleColor = vec3<f32>(1.0, 0.8118, 0.3255);
-            let bottomCircleColor = vec3<f32>(1.0, 0.0, 0.9882);
-            let circleCenter = vec2<f32>(0.5, 0.5);
-            let circleRadius = 0.375;
-            var color = vec4<f32>(0.0, 0.0, 0.0, 1.0);
+            const height = 480.0;
+
+            const topBackgroundColor = vec3<f32>(0.5255, 0.4275, 1.0);
+            const bottomBackgroundColor = vec3<f32>(0.0, 0.0, 0.5176);
+            const topCircleColor = vec3<f32>(1.0, 0.8118, 0.3255);
+            const bottomCircleColor = vec3<f32>(1.0, 0.0, 0.9882);
+
+            const circleCenter = vec3<f32>(0.5, 0.5, 0.0);
+            const circleRadius = 0.375;
+            const rectangleCenters = array<vec3<f32>, 5>(vec3<f32>(0.0, 0.45, 0.0),
+                                                         vec3<f32>(0.0, 0.5, 0.0),
+                                                         vec3<f32>(0.0, 0.57, 0.0),
+                                                         vec3<f32>(0.0, 0.66, 0.0),
+                                                         vec3<f32>(0.0, 0.75, 0.0));
+            const rectangleHalfSizes = array<vec3<f32>, 5>(vec3<f32>(1.0, 0.007, 1.0),
+                                                           vec3<f32>(1.0, 0.01, 1.0),
+                                                           vec3<f32>(1.0, 0.015, 1.0),
+                                                           vec3<f32>(1.0, 0.02, 1.0),
+                                                           vec3<f32>(1.0, 0.03, 1.0));
             
-            let backgroundT = FragCoord.y / height;
-            let gradientBackground = mix(topBackgroundColor, bottomBackgroundColor, backgroundT);
+            let normalizedCoordinates = FragCoord.xyz / vec3<f32>(height, height, 1.0);
 
-            let normalizedCoordinates = FragCoord.xy / vec2<f32>(height, height);
-            let sdf = length(normalizedCoordinates - circleCenter) - circleRadius;
+            var dist = computeSDFSphere(normalizedCoordinates, circleCenter, circleRadius);
 
-            if (sdf < 0.0) {
-                let dynamicYield = min(50.0, FragCoord.y / 20.0);
-                let circleT = FragCoord.y / height / 1.2;
-                let gradientCircle = mix(topCircleColor, bottomCircleColor, circleT);
-                if (FragCoord.y > (height / 2.5)) {
-                    if (FragCoord.y % 42.0 > dynamicYield) {
-                        color = vec4<f32>(gradientCircle, 1.0);
-                    } else {
-                        color = vec4<f32>(gradientBackground, 1.0);
-                    }
-                } else {
-                    color = vec4<f32>(gradientCircle, 1.0);
-                }
-            } else {
-                color = vec4<f32>(gradientBackground, 1.0);
+            for (var i: u32 = 0u; i < 5; i = i + 1u) {
+                dist = max(dist, -computeSDFRectangle(normalizedCoordinates, rectangleCenters[i], rectangleHalfSizes[i]));
+            }
+
+            let backgroundColor = computeBackgroundColor(FragCoord.y, height, topBackgroundColor, bottomBackgroundColor);
+            let circleColor = computeBackgroundColor(FragCoord.y, height, topCircleColor, bottomCircleColor);
+
+            var color = backgroundColor;
+            if (dist < 0.0) {
+                color = circleColor;
             }
             return color;
         }`;
